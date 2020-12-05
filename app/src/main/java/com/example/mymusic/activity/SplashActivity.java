1e0319afd06a36e26bb6ba203744cb2585d2ee8a
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-12-05 11:23:08 +0800

    Update SplashActivity.java
    实现启动界面全屏

--- app/src/main/java/com/example/mymusic/activity/SplashActivity.java
+++ app/src/main/java/com/example/mymusic/activity/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.mymusic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mymusic.R;
 
@@ -15,5 +17,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //设置界面全屏
+        //获取decorView
+        View decorView=getWindow().getDecorView();
+        //判断版本
+        if(Build.VERSION.SDK_INT>11&&Build.VERSION.SDK_INT<19)
+        {
+            //11-18
+            decorView.setSystemUiVisibility(View.GONE);
+        }else if(Build.VERSION.SDK_INT>=19)
+        {
+            //19以及以上版本
+            //SYSTEM_UI_FLAG_HIDE_NAVIGATION:隐藏导航栏
+            //SYSTEM_UI_FLAG_IMMERSIVE_STICKY:从状态栏下拉会半透明悬浮显示一会儿状态栏和导航栏
+            //SYSTEM_UI_FALG_FUULLSCREEN:全屏
+            int options=View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            //设置到控件
+            decorView.setSystemUiVisibility(options);
+        }
     }
 }
