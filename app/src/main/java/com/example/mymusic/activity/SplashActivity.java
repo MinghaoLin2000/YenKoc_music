e5fa31a6afac97fed96019ab9ada8208378e275b
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-12-05 11:08:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mymusic/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.mymusic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mymusic.R;
+
+/**
+ * 启动界面
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
