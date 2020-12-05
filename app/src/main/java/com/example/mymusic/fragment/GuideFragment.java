db2727c5bb9355336db09ff0f648f08729f4ba2e
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-12-05 18:32:05 +0800

    1

---
+++ app/src/main/java/com/example/mymusic/fragment/GuideFragment.java
@@ -0,0 +1,61 @@
+package com.example.mymusic.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mymusic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GuideFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GuideFragment extends Fragment {
+
+
+    public GuideFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     *  创建方法
+     * @return
+     */
+    public static GuideFragment newInstance() {
+        GuideFragment fragment = new GuideFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /**
+     * 创建
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    /**
+     * 返回要显示的view
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_guide, container, false);
+    }
+}
