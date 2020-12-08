144363fcea9bd8e07444de79a4d9b4696d3342d8
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-12-08 19:21:10 +0800

    11

--- app/src/main/java/com/example/mymusic/util/StringUtil.java
+++ app/src/main/java/com/example/mymusic/util/StringUtil.java
@@ -10,6 +10,15 @@ public class StringUtil {
      */
     public static boolean isPhone(String value)
     {
-        return false;
+        return value.matches(REGEX_PHONE);
     }
+    /**
+     * 手机号正则表达式
+     * 移动：134 135 136 137 138 139 147 150 151 152 157 158 159 178 182 183 184 187 188 198
+     * 联通：130 131 132 145 155 156 166 171 175 176 185 186
+     * 电信：133 149 153 173 177 180 181 189 199
+     * 虚拟运营商: 170
+     */
+    public static final String REGEX_PHONE = "^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$";
+
 }
--- app/src/test/java/com/example/mymusic/util/StringUtilTest.java
+++ app/src/test/java/com/example/mymusic/util/StringUtilTest.java
@@ -2,6 +2,9 @@ package com.example.mymusic.util;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * 字符串工具类测试
  */
@@ -12,7 +15,17 @@ public class StringUtilTest {
     @Test
     public void testIsPhone()
     {
-        
+        //这是一个正确的手机号格式
+        //所以用断言判断结果为true
+        //只有结果为true才表示测试通过
+        //也就表示我们的代码没问题
+
+        //这里的assert其实是junit中的
+        assertTrue(StringUtil.isPhone("13141111111"));
+
+        //这是一个错误的手机号格式
+        //所以用断言判断结果为false
+        assertFalse(StringUtil.isPhone("ixuea"));
     }
 
 }
